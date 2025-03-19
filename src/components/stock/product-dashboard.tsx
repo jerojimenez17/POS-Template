44b08aa60e40aaa1d6323479d4b26aaa0baa3fa0
jerojimenez17 <jerodepiran@gmail.com>
2025-03-19 02:27:11 -0300

    remove sigin button

--- src/components/stock/product-dashboard.tsx
+++ src/components/stock/product-dashboard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import { useEffect, useState } from "react";
@@ -32,7 +33,7 @@ const ProductDashboad = ({ session }: props) => {
   }, []);
 
   const router = useRouter();
-  return session?.user ? (
+  return (
     <div className="flex flex-col h-full w-full items-center">
       <Modal
         visible={openModal}
@@ -54,17 +55,6 @@ const ProductDashboad = ({ session }: props) => {
         descriptionFilter={descriptionFilter}
         products={products || []}
       />
-    </div>
-  ) : (
-    <div className="h-screen text-center  items-center flex justify-center text-pink-400 mx-auto overflow-auto  sm:w-screen-sm mb-10">
-      <div className="justify-center align-middle">
-        <button
-          className="p-2 px-3 align-middle justify-center my-auto hover:shadow-sm rounded-full hover:shadow-pink-300 bg-linear-to-r from-rose-200 to-pink-200 hover:text-white font-semibold"
-          onClick={() => router.push("/login")}
-        >
-          Iniciar Sesion
-        </button>
-      </div>
     </div>
   );
 };
