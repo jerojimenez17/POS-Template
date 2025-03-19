1402d42bc5e4f98d10ad6057785dcc4cd6878ff4
jerojimenez17 <jerodepiran@gmail.com>
2025-03-19 02:34:24 -0300

    remove signin button

--- src/components/stock/product-dashboard.tsx
+++ src/components/stock/product-dashboard.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import ProductForm from "./product-form";
 import StockFilterPanel from "./stock-filter-panel";
 import Modal from "../Modal";
-import { useRouter } from "next/navigation";
 import { Session } from "next-auth";
 import ProductDataTable from "../ProductDataTable";
 import Product from "@/models/Product";
@@ -16,6 +15,7 @@ interface props {
   session: Session | null;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ProductDashboad = ({ session }: props) => {
   const [openModal, setOpenModal] = useState(false);
   const [descriptionFilter, setDescriptionFilter] = useState("");
@@ -31,8 +31,7 @@ const ProductDashboad = ({ session }: props) => {
     });
   }, []);
 
-  const router = useRouter();
-  return session?.user ? (
+  return (
     <div className="flex flex-col h-full w-full items-center">
       <Modal
         visible={openModal}
@@ -54,17 +53,6 @@ const ProductDashboad = ({ session }: props) => {
         descriptionFilter={descriptionFilter}
         products={products || []}
       />
-    </div>
-  ) : (
-    <div className="h-screen text-center  items-center flex justify-center text-pink-400 mx-auto overflow-auto  sm:w-screen-sm mb-10">
-      <div className="justify-center align-middle">
-        <button
-          className="p-2 px-3 align-middle justify-center my-auto hover:shadow-sm rounded-full hover:shadow-pink-300 bg-linear-to-r from-rose-200 to-pink-200 hover:text-white font-semibold"
-          onClick={() => router.push("/login")}
-        >
-          Iniciar Sesion
-        </button>
-      </div>
     </div>
   );
 };
