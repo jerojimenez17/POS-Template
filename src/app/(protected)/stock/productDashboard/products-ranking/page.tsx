f49df61bcb8cee1c781c8c072bd7c2ccf4de0c17
jerojimenez17 <jerodepiran@gmail.com>
2025-06-16 02:07:06 -0300

    wraping component with suspense

--- src/app/(protected)/stock/productDashboard/products-ranking/page.tsx
+++ src/app/(protected)/stock/productDashboard/products-ranking/page.tsx
@@ -2,11 +2,15 @@
 
 import PickDateStockRanking from "@/components/stock/pick-date-stock-ranking";
 import ProductRanking from "@/components/stock/product-ranking";
+import { Suspense } from "react";
 
 const page = () => {
   return (
     <div className="h-full w-full">
-      <PickDateStockRanking />
+      <Suspense fallback={<div>Cargando selector de mes...</div>}>
+        <PickDateStockRanking />
+      </Suspense>
+
       <ProductRanking />
     </div>
   );
