486cd73bbd103b126ae547824b81b1daec55f044
jerojimenez17 <jerodepiran@gmail.com>
2026-03-13 11:03:38 -0300

    fix configuration error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: "class", // 👈 clave
+export default {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
